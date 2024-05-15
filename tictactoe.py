414919bb20ea051db8c256398a5e8e661d9aed8a
Will Wallace <wwranchman66@gmail.com>
2024-05-14 19:07:16 -0500

    Implemented main menu and Player vs. Player mode

--- tictactoe.py
+++ tictactoe.py
@@ -9,7 +9,7 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 
-# PROPORTIONS AND SIZES
+# GAME PROPORTIONS AND SIZES
 WIDTH = 300
 HEIGHT = 300
 LINE_WIDTH = 5
@@ -23,7 +23,7 @@ CROSS_WIDTH = 25
 # TIC TAC TOE BOARD
 board = np.zeros((BOARD_ROWS, BOARD_COLS))
 
-# FUNCTIONS
+# GAMEPLAY FUNCTIONS
 def draw_lines(color=WHITE):
     for i in range(1, BOARD_ROWS):
         pygame.draw.line(screen, color, (0, SQUARE_SIZE * i), (WIDTH, SQUARE_SIZE * i), LINE_WIDTH)
@@ -124,10 +124,9 @@ def best_move():
         return True
     return False
 
-# MAIN GAME LOOP
-if __name__ == '__main__':
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+# TIC TAC TOE GAME LOOP
+# vsAI is a boolean that indicates whether the player is facing against the AI or not
+def game_loop(vsAI):
     pygame.display.set_caption('Tic Tac Toe')
     screen.fill(BLACK)
     draw_lines()
@@ -151,7 +150,7 @@ if __name__ == '__main__':
                     currentPlayer = currentPlayer % 2 + 1
 
                     # Make AI move if in AI mode and if game is not over
-                    if not game_over:
+                    if vsAI and not game_over:
                         if best_move():
                             if check_win(2):
                                 game_over = True
@@ -161,10 +160,12 @@ if __name__ == '__main__':
                         game_over = True
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
+                if event.key == pygame.K_r or event.key == pygame.K_ESCAPE:
                     restart_game()
                     game_over = False
                     currentPlayer = 1
+                    if event.key == pygame.K_ESCAPE:
+                        main_menu()
 
         if not game_over:
             draw_figures()
@@ -180,3 +181,80 @@ if __name__ == '__main__':
                 draw_lines(GRAY)
         
         pygame.display.update()
+
+# MAIN MENU PROPERTIES
+TITLE_TEXT = 'Tic Tac Toe'
+TITLE_TEXT_SIZE = 50
+TITLE_TEXT_POS = (150, 30) # (center)
+MENU_FONT = 'Calibri'
+
+MENU_BUTTON_TEXT_SIZE = 24
+MENU_BUTTON_WIDTH = 200
+MENU_BUTTON_HEIGHT = 80
+
+PVP_BUTTON_POS = (50, 70) # (top left corner)
+PVP_TEXT = 'Player vs. Player'
+PVP_TEXT_POS = (150, 110)
+
+AI_BUTTON_POS = (50, 170) # (top left corner)
+AI_TEXT = 'Player vs. AI'
+AI_TEXT_POS = (150, 210)
+
+INFO_TEXT = 'R = Reset Board  |  ESC = Main Menu'
+INFO_TEXT_SIZE = 15
+INFO_TEXT_POS = (150, 280) # (center)
+
+# MAIN MENU FUNCTIONS
+def draw_rectangle(top_left, rect_width, rect_height):
+    pygame.draw.line(screen, WHITE, top_left, (top_left[0] + rect_width, top_left[1]), LINE_WIDTH)
+    pygame.draw.line(screen, WHITE, top_left, (top_left[0], top_left[1] +  rect_height), LINE_WIDTH)
+    pygame.draw.line(screen, WHITE, (top_left[0], top_left[1] +  rect_height), (top_left[0] + rect_width, top_left[1] + rect_height), LINE_WIDTH)
+    pygame.draw.line(screen, WHITE, (top_left[0] + rect_width, top_left[1]), (top_left[0] + rect_width, top_left[1] + rect_height), LINE_WIDTH)
+
+def render_text(text, text_size, font, position):
+    surface = pygame.font.SysFont(font, text_size).render(text, True, WHITE)
+    screen.blit(surface, surface.get_rect(center=position))
+
+def within_rect(pos, top_left, rect_width, rect_height):
+    if pos[0] in range(top_left[0], top_left[0] + rect_width) and pos[1] in range(top_left[1], top_left[1] + rect_height):
+        return True
+    return False
+
+# MAIN MENU LOGIC LOOP
+def main_menu():
+    pygame.display.set_caption('Main Menu')
+    screen.fill(BLACK)
+
+    # Render title text on screen
+    render_text(TITLE_TEXT, TITLE_TEXT_SIZE, MENU_FONT, TITLE_TEXT_POS)
+
+    # Render play buttons on screen (vs. Player and vs. AI)
+    draw_rectangle(PVP_BUTTON_POS, MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT)
+    draw_rectangle(AI_BUTTON_POS, MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT)
+    render_text(PVP_TEXT, MENU_BUTTON_TEXT_SIZE, MENU_FONT, PVP_TEXT_POS)
+    render_text(AI_TEXT, MENU_BUTTON_TEXT_SIZE, MENU_FONT, AI_TEXT_POS)
+
+    # Render info text
+    render_text(INFO_TEXT, INFO_TEXT_SIZE, MENU_FONT, INFO_TEXT_POS)
+
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouseX = event.pos[0]
+                mouseY = event.pos[1]
+                if within_rect((mouseX, mouseY), PVP_BUTTON_POS, MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT):
+                    # Start Player vs. Player game
+                    game_loop(False)
+                elif within_rect((mouseX, mouseY), AI_BUTTON_POS, MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT):
+                    # Start Player vs. AI game
+                    game_loop(True)
+
+        pygame.display.update()
+
+# MAIN
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    main_menu()
